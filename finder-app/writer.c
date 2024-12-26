ed8eaf39b32d8946eec46014b5a0d8ce5f7d1a0b
DanMekler <danmekler12@gmail.com>
2024-12-26 09:27:51 +0200

    writer c file

---
+++ finder-app/writer.c
@@ -0,0 +1,46 @@
+#include <fcntl.h>
+#include <syslog.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+
+    openlog("writer", LOG_PID, LOG_USER);
+
+    if (argc < 3){
+        syslog(LOG_ERR, "Error: both arguments are required");
+        printf("Error: both arguments are required\n");
+        closelog();
+        return 1;
+    }
+
+    char* writefile = argv[1];
+    char* writestr = argv[2];
+
+    int fd = open(writefile, O_WRONLY | O_TRUNC);
+    if (fd == -1){
+        syslog(LOG_ERR, "open falied: %s", strerror(errno));
+        printf("open failed: %s", strerror(errno));
+        closelog();
+        return 1;
+    }
+
+    int write_bytes = write(fd, writestr, strlen(writestr));
+    if (write_bytes == -1){
+        syslog(LOG_ERR, "write falied: %s", strerror(errno));
+        printf("write failed: %s", strerror(errno));
+        close(fd);
+        closelog();
+        return 1;
+    }
+
+    syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
+    printf("Writing %s to %s\n", writestr, writefile);
+
+    close(fd);
+    closelog();
+    return 0;
+
+}
