2f9bf8cd1d68c2ee777039432d05f2167ef645dd
DanMekler <danmekler12@gmail.com>
2025-01-06 10:46:48 +0200

    10:46

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/assignment4-result.txt
-username=$(cat conf/username.txt)
+username=$(cat /home/dan/assignment-4-DanMekler/buildroot/output/target/etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /home/dan/assignment-4-DanMekler/buildroot/output/target/etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
