0a213d1b61e51e0b63258c47f2ccbb04fb2f6a76
DanMekler <danmekler12@gmail.com>
2025-01-12 08:30:31 +0200

    12/01/2025 08:30

--- server/aesdsocket-start-stop.d
+++ server/aesdsocket-start-stop.d
@@ -4,15 +4,15 @@ case "$1" in
     start)
         echo "Starting aesdsocket"
         start-stop-daemon -S -n aesdsocket -a ~/server
-    ;;
+        ;;
     stop)
         echo "Stopping aesdsocket"
         start-stop-daemon -K -n aesdsocket
-    ;;
+        ;;
     *)
         echo "Usage: $0 {start|stop}"
         exit 1
-    ;;
-exac
+        ;;
+esac
 
 exit 0
